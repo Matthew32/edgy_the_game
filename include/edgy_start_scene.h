d77905d0a78ce92027c8534f5aea2044acafe1f4
Matthew32 <mmartinezmateo93@gmail.com>
2024-06-26 23:18:03 +0200

    Added start_scene  class

---
+++ include/edgy_start_scene.h
@@ -0,0 +1,13 @@
+#ifndef EDGY_START_SCENE_H
+#define EDGY_START_SCENE_H
+#include "edgy_hanamin.h"
+namespace edgy
+{
+    class start_scene
+    {
+        public:
+        void scene();
+        void nextText();
+    };
+};
+#endif
---
+++ src/edgy_start_scene.cpp
@@ -0,0 +1,15 @@
+#include "edgy_start_scene.h"
+
+namespace edgy
+{
+    int selectedText = 0;
+
+    void start_scene::scene() {
+
+    };
+
+    void start_scene::nextText()
+    {
+        selectedText++;
+    }
+};
